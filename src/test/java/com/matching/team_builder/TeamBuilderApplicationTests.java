afe6ea40df3feda8e2b5f317fd6d9764a82abb7c
Na Young Jun <phr19656@naver.com>
2026-03-10 15:37:25 +0900

    chore: Spring Boot 초기 프로젝트 뼈대 세팅

---
+++ src/test/java/com/matching/team_builder/TeamBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matching.team_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

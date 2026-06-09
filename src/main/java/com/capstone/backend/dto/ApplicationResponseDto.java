e22cbaf63bc4bbe0d9bc4f808f66799a7120cb44
Muwon <kangmuwon6642@naver.com>
2026-06-09 14:37:48 +0900

    test

--- src/main/java/com/capstone/backend/dto/ApplicationResponseDto.java
+++ src/main/java/com/capstone/backend/dto/ApplicationResponseDto.java
@@ -1,4 +1,6 @@
 package com.capstone.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty; 
 import lombok.Builder;
 import lombok.Getter;
 import java.time.LocalDateTime;
@@ -6,14 +8,20 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class ApplicationResponseDto {
+    
+    @JsonProperty("application_id") // 👈 2. 리액트의 app.application_id와 매칭
     private Long applicationId;
+    
     private String supportRole;
     private String message;
     private String status;
     private Long applicantId;
-    private String applicantName;  // ← 추가
+    private String applicantName;  
+    
+    @JsonProperty("project_id")     // 👈 3. 리액트의 app.project_id와 매칭
     private Long projectId;
-    private String projectTitle;   // ← 추가
+    
+    private String projectTitle;  
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }

a4c88ce9471188d4ce91d8bf00b2734015fad1db
Muwon <kangmuwon6642@naver.com>
2026-06-09 22:22:32 +0900

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080"; // 추후 변경
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createPost = async (data) => {
   const response = await axios.post(`${BASE_URL}/posts`, data);
